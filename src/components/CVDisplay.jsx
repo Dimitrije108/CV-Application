bd7abb21984ded03f5857f8b79b836be0bfaa62a
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-09-27 16:23:42 +0200

    feat: refactor cv display component structure for better separation of concern - separate different components

--- src/components/CVDisplay.jsx
+++ src/components/CVDisplay.jsx
@@ -1,87 +1,91 @@
 import '../styles/CVDisplay.css'
 
-export default function CVDisplay({ personalData, educationList, workHistoryList, skillList }) {
+export default function CVDisplay({ 
+	personalData, 
+	educationList, 
+	workHistoryList, 
+	skillList 
+}) {
 	return (
 		<section className="cv-display">
-			<div className="cv-personal-details">
-				<div className="full-name">
-					{personalData.firstName || "Nikola"}{' '}{personalData.lastName || "Jokić"}
-				</div>
-				<div className="job-title">
-					{personalData.jobTitle || "Horse racer"}
-				</div>
-				<div className="contact-info">
-					{personalData.email || "somborshuffle15@example.com"}
-					{personalData.phone || "065/555-333"}
-					{personalData.city || "Sombor"}
-					{personalData.country || "Serbia"}
-				</div>
-			</div>
-			<div className="cv-education">
-				{educationList.map((item) => (
-					<div>
-						<h3>{item.degree}</h3>
-						<p>{item.school}</p>
-						<p>{item.city}</p>
-						<div className="education-dates-cont">
-							<div>{item.startDate}</div>
-							<div>{item.endDate}</div>
-						</div>
-					</div>
-				))}
-			</div>
-			<div className="cv-work-history">
-				{workHistoryList.map((item) => (
-					<div>
-						<h3>{item.jobTitle}</h3>
-						<p>{item.company}</p>
-						<p>{item.location}</p>
-						<div className="education-dates-cont">
-							<div>{item.startDate}</div>
-							<div>{item.endDate}</div>
-						</div>
-						<p>{item.jobDescription}</p>
-					</div>
-				))}
-			</div>
-			<div className="cv-skills">
-				{skillList.map((skill) => (
-						<p>{skill}</p>
-					))}
-			</div>
+			<CVPersonalDetails
+				data={personalData}
+			/>
+			<CVEducation
+				educationList={educationList}
+			/>
+			<CVWorkHistory
+				workHistoryList={workHistoryList}
+			/>
+			<CVSkills
+				skillList={skillList}
+			/>
 		</section>
 	)
 }
 
-function CVEducation() {
+function CVPersonalDetails({ data }) {
 	return (
-		<>
-			{Degree}
-			{School}
-			{startDate}
-			{endDate}
-			{City}
-		</>
+		<div className="cv-personal-details">
+			<div className="full-name">
+				{data.firstName || "Nikola"}{' '}{data.lastName || "Jokić"}
+			</div>
+			<div className="job-title">
+				{data.jobTitle || "Horse racer"}
+			</div>
+			<div className="contact-info">
+				{data.email || "somborshuffle15@example.com"}
+				{data.phone || "065/555-333"}
+				{data.city || "Sombor"}
+				{data.country || "Serbia"}
+			</div>
+		</div>
 	)
 }
 
-function CVWorkHistory() {
+function CVEducation({ educationList }) {
 	return (
-		<>
-			{jobTitle}
-			{company}
-			{location}
-			{startDate}
-			{endDate}
-			{jobDescription}
-		</>
+		<div className="cv-education">
+			{educationList.map((item) => (
+				<div>
+					<h3>{item.degree}</h3>
+					<p>{item.school}</p>
+					<p>{item.city}</p>
+					<div className="education-dates-cont">
+						<div>{item.startDate}</div>
+						<div>{item.endDate}</div>
+					</div>
+				</div>
+			))}
+		</div>
 	)
 }
 
-function CVSkills() {
+function CVWorkHistory({ workHistoryList }) {
 	return (
-		<>
-			{skill}
-		</>
+		<div className="cv-work-history">
+			{workHistoryList.map((item) => (
+				<div>
+					<h3>{item.jobTitle}</h3>
+					<p>{item.company}</p>
+					<p>{item.location}</p>
+					<div className="education-dates-cont">
+						<div>{item.startDate}</div>
+						<div>{item.endDate}</div>
+					</div>
+					<p>{item.jobDescription}</p>
+				</div>
+			))}
+		</div>
+	)
+}
+
+function CVSkills({ skillList }) {
+	return (
+		<div className="cv-skills">
+			{skillList.map((skill) => (
+				<p>{skill}</p>
+			))}
+		</div>
 	)
 }
